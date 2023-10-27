dc4e3051c277edbc0a73c36826d114828757ed69
DESKTOP-D5NS5P4\manuel <manuelclarobarba@gmail.com>
2023-10-27 13:18:20 +0200

    Agregar archivos de proyecto.

---
+++ JCB_Cuadrado/Controladores/Program.cs
@@ -0,0 +1,25 @@
+﻿using JCB_Cuadrado.Servicios;
+
+namespace Ejercicio2NumCuadrado.Controladores
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PedirNumeroIntfz num = new PedirNumeroImpl();
+            double numPedido, raiz;
+
+
+            numPedido = num.pedirNumero();
+            raiz = Math.Sqrt(numPedido);
+
+            if (raiz * raiz == numPedido)
+                Console.WriteLine("El numero es entero");
+            else
+                Console.WriteLine("El numero no es entero");
+
+        }
+    }
+}
---
+++ JCB_Cuadrado/Servicios/PedirNumeroImpl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JCB_Cuadrado.Servicios
+{
+     internal class PedirNumeroImpl : PedirNumeroIntfz
+    {
+
+        public double pedirNumero()
+        {
+            double numeroPedido;
+            Console.WriteLine("Escriba un nuemro entero ");
+            numeroPedido = Convert.ToDouble(Console.ReadLine());
+
+            return numeroPedido;
+        }
+    }
+}
